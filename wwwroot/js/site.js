3bca9dc66e6469d544f9532858556d67ad402521
Andrew Yemtsev <59855700+yemtseva@users.noreply.github.com>
2020-09-18 11:37:53 -0400

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿
+function animatePath(shortestPath) {
+    document.write("hello");
+    for (let i = 0; i < shortestPath.length; i++) {
+        setTimeout(() => {
+            shortestPath[i].className = 'node path';
+
+        }, 15 * i);
+    }
+}
